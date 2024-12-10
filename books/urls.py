1516dfc82d2c4f4dd295658ff78b34ed77b13324
Harry Miles <harrypetermiles@gmail.com>
2024-12-10 11:49:50 +0000

    feat: add index home page content

--- books/urls.py
+++ books/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('logout', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
-    path('login/', views.login_view, name="login")
+    path('login/', views.login_view, name='login')
 ]
