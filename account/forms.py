60673272c7aad6d625f1787ba7583ec19ea882b1
Harry Miles <harrypetermiles@gmail.com>
2024-12-22 11:52:11 +0000

    feat: add docstrings to account/forms.py and books/forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,8 +1,15 @@
+"""Django forms file for forms utilised in account/views.py.
+
+Forms:
+RegisterForm -- a form for user registration on the register page.
+"""
+
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
+    """A form for user registration."""
     class Meta:
         model = User
         fields = ['username','email','password1','password2']  
--- books/forms.py
+++ books/forms.py
@@ -1,12 +1,22 @@
+"""Django forms file for forms utilised in books/views.py.
+
+Forms:
+ManageForm -- a form for managing the title and author of a UserBook
+              on the manage book page.
+URLForm -- a form for submitting a URL on the add book page.
+"""
+
 from django import forms
 from .models import Book, UserBook
 
 class ManageForm(forms.ModelForm):
+    """A form for managing the title and author of a UserBook"""
     class Meta:
         model = UserBook
         fields = ['title', 'author']
 
 class URLForm(forms.ModelForm):
+    """A form for submitting a URL when adding a book."""
     class Meta:
         model = Book
         fields = ('url',)
