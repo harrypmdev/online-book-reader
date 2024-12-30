085aa8da818d86d192b448ea60b81986f5c343a0
Harry Miles <harrypetermiles@gmail.com>
2024-12-30 02:52:43 +0000

    fix: set debug to False for deployment

--- online_book_reader/settings.py
+++ online_book_reader/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-harrypmdev-onlinebookre-cnoq0b3xu2q.ws.codeinstitute-ide.net",
